e2bf74b65f2cc454e92eea478328bf37e0a8f26d
erikisakss <112391000+erikisakss@users.noreply.github.com>
2022-09-07 23:57:31 +0200

    Lab3Upg1

---
+++ Lab3Upg1/Lab3Upg1/Lab3Upg1/Lab3Upg1.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+
+
+
+float currencyConverter(float sek)
+{
+	
+	float dollarValue = sek / 10.7;
+
+	return dollarValue;
+
+
+}
+
+
+
+	int main(void)
+	{
+
+		int sek;
+		float dollar;
+
+		printf("SEK to $ Converter\n\n");
+		printf("Enter how many SEK you want to conver to $: ");
+		scanf_s("%d", &sek);
+
+		dollar = currencyConverter(sek);
+
+		printf("%d SEK = %.2f$", sek, dollar);
+
+		return 0;
+	}
+	
+
+
+
+
