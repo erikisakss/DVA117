31dc7ed7626aba357158c779903bf9d9a413dae3
erikisakss <112391000+erikisakss@users.noreply.github.com>
2022-09-19 11:04:18 +0200

    tt

--- Lab4Upg1/Lab4Upg1/Lab4Upg1.cpp
+++ Lab4Upg1/Lab4Upg1/Lab4Upg1.cpp
@@ -4,8 +4,6 @@
 #define SIZE 10
 
 
-
-
 void funk(int slumpf[])
 {
 
@@ -23,42 +21,28 @@ int countElement(int inputArray[], int elementToCount) {
 		if (inputArray[i] == elementToCount)
 		{
 			count++;
-
-
 		}
 
-
-		
-
-
 	}
-
 
 	return count;
 }
 
 
-
-
 int main(void) {
-
 
 	int slump[10], i, searchedNumber, amountTime;
 	funk(slump);
-	
 	
 	for (i = 0; i < 10; i++) {
 		printf("Random Number: %d\n", slump[i]);
 
 	}
 
-
 	printf("Number you want to search for: ");
 		scanf_s("%d", &searchedNumber);
 
-
 		amountTime = countElement(slump, searchedNumber);
-
 
 		printf("The number %d was found %d times!\n", searchedNumber, amountTime);
 
